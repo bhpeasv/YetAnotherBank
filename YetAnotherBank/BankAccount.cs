af5d36d923845b3d495e8aedbcdc81679af3bf8f
bhp <bhp@easv.dk>
2018-11-15 22:21:52 +0100

    Added feature Deposit for BankAccount with Deposit tests in the BankAccountTest class.

--- YetAnotherBank/BankAccount.cs
+++ YetAnotherBank/BankAccount.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace YetAnotherBank
 {
@@ -18,6 +16,23 @@ namespace YetAnotherBank
         public BankAccount(int id)
         : this(id, 0.0)
         {
+        }
+
+        /// <summary>
+        /// Adds the <paramref name="amount"/> to the balance of the bank account.
+        /// If the <paramref name="amount"/> is not greater than zero, an ArgumentException is thrown.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <exception cref="ArgumentException">
+        /// Thrown is the <paramref name="amount"/> is not greater than zero
+        /// </exception>
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Amount must be greater than zero");
+            }
+            Balance += amount;
         }
     }
 }
