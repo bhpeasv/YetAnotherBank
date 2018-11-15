06047a0e7ea02dfe4d31e017056c4a8da1f44e93
bhp <bhp@easv.dk>
2018-11-15 09:19:13 +0100

    Add project files.

---
+++ YetAnotherBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YetAnotherBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
